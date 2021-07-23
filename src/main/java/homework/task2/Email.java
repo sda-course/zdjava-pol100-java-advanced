4e95656216d529c1f61a7ae282433f87c21bc3fb
cs <siwon.cezary@gmail.com>
2021-07-23 16:10:52 +0200

    email regular expression fixed

--- src/main/java/homework/task2/Email.java
+++ src/main/java/homework/task2/Email.java
@@ -1,8 +1,13 @@
 package homework.task2;
 
+import day1.model.Panel;
+
+import java.util.regex.Pattern;
+
 public class Email extends Message {
     final String emailAddress;
     final String subject;
+    private final static Pattern emailPattern = Pattern.compile("[a-zA-Z]{3,}@[a-z]{2,}\\.[a-z.]{2,}");
 
     public Email(String content) {
         super(content);
@@ -34,7 +39,9 @@ public class Email extends Message {
         if (emailAddress == null || emailAddress.isEmpty() || emailAddress.length() < 5) {
             return false;
         }
-        return emailAddress.matches("[a-zA-Z]{3,}@[a-z]{2,}\\.[a-z.]{2,}");
+        //return emailAddress.matches("[a-zA-Z]{3,}@[a-z]{2,}\\.[a-z.]{2,}");
+        //lub lepiej, aby nie kompilowć wyrażenia regularnego z każdym wywołaniem
+        return emailPattern.matcher(emailAddress).matches();
     }
 
     private boolean isValidSubject() {
